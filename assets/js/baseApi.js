fb7cb7b8093f1e760b2f946b48566ecfd84c3e42
lihao <2677343997@qq.com>
2021-04-03 21:01:17 +0800

    完成了主页功能

--- assets/js/baseApi.js
+++ assets/js/baseApi.js
@@ -2,4 +2,24 @@
 $.ajaxPrefilter(function(options) {
     //    拿到发起ajax请求时的URL，然后拼接上根路径
     options.url = 'http://ajax.frontend.itheima.net' + options.url
+        // 给需要权限的请求统一设置请求头
+    if (options.url.indexOf('/my/' !== -1)) {
+        options.headers = {
+            //    设置请求头
+            //    从浏览器中取出存入的token
+            Authorization: localStorage.getItem('token') || ''
+        }
+    }
+    // 最终都会执行的函数completa
+    // 配置全局的权限管理
+    options.complete = function(rel) {
+        if (rel.responseJSON.status == 1) {
+            // 清空token
+            localStorage.removeItem('token')
+                // 强制跳转到登录页面
+            location.href = '/login.html'
+
+        }
+    }
+
 })
